78ba4454b0682078712fd5fe07021868b65937c5
codingrot17 <ademolaemmanuel645@gmail.com>
2025-10-24 09:21:07 +0100

    removed backend directory

--- backend/models/service.model.js
+++
@@ -1,6 +0,0 @@
-const Datastore = require('nedb');
-const path = require('path');
-
-const db = new Datastore({ filename: path.join(__dirname, '../db/services.db'), autoload: true });
-
-module.exports = db;
